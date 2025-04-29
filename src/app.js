36c5700f2b001f796c1583d5e52c3cba774dd4e8
roble111 <struckgold888@gmail.com>
2025-04-29 01:33:45 +0000

    Finish prject

--- src/app.js
+++ src/app.js
@@ -1,11 +1,47 @@
 import "bootstrap";
 import "./style.css";
 
-
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
+window.onload = function () {
+  let who = ["The dog", "My grandma", "the mailman", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "my phone", "the car"];
+  let when = [
+    "before the class",
+    "when I was sleeping",
+    "while I was exercising",
+    "during my lunch",
+    "while I was praying",
+  ];
+  const excuse = document.querySelector("#excuse");
+  function randomIndex(array) {
+    return Math.floor(Math.random() * array.length);
+  }
+  let whoIndex = randomIndex(who);
+  let actionIndex = randomIndex(action);
+  let whatIndex = randomIndex(what);
+  let whenIndex = randomIndex(when);
+
+  excuse.innerHTML =
+    who[whoIndex] +
+    " " +
+    action[actionIndex] +
+    " " +
+    what[whatIndex] +
+    " " +
+    when[whenIndex];
+
   console.log("Hello Rigo from the console!");
+  console.log(
+    who[whoIndex] +
+      " " +
+      action[actionIndex] +
+      " " +
+      what[whatIndex] +
+      " " +
+      when[whenIndex]
+  );
+  console.log(excuse.innerHTML);
 };
